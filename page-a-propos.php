347198f14ad728d1e773277d1aa149a8ba97a198
Basile Vanhaverbeke <basile.vanha@outlook.fr>
2024-02-01 16:19:10 -0400

    [Integration] - Ateliers & spectacle

--- page-a-propos.php
+++ page-a-propos.php
@@ -7,6 +7,16 @@
 $context                = Timber::context();
 $context['post']        = Timber::get_post();
 
+$membres = array();
+$items = get_field('a-propos-equipe', $context['post']->ID)['membres-liste'];
+
+for ($i = 0; $i < count($items) ; $i++) {
+    $postID = url_to_postid($items[ $i ]);
+    $membres[$i] = Timber::get_post($postID);
+}
+
+$context['membres'] = $membres;
+
 $context['hero']        = get_field('a-propos-hero',        $context['post']->ID);
 $context['groupe']      = get_field('a-propos-groupe',      $context['post']->ID);
 $context['equipe']      = get_field('a-propos-equipe',      $context['post']->ID);
